0bc78376156c875fd0febc58df16a49ed0dd4260
hehoku <hehoku@outlook.com>
2023-01-25 23:10:24 +0800

    feat: use real date fill

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,52 @@
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "./page.module.css";
+"use client";
 
-const inter = Inter({ subsets: ["latin"] });
+import styles from "./page.module.css";
+import { useEffect, useState } from "react";
+
+interface datesOfYearType {
+  [month: string]: string[];
+}
 
 export default function Home() {
+  let [datesOfYear, setDatesOfYear] = useState<datesOfYearType>({});
+
+  useEffect(() => {
+    let now = new Date();
+    let nowYear = now.getFullYear();
+    let firstDayOfThisYear = new Date(nowYear, 0, 1);
+
+    let dates: { [month: string]: string[] } = {};
+
+    for (let i = 0; i <= 365; i++) {
+      let date = new Date(new Date(firstDayOfThisYear.getTime()).setDate(+i));
+      let month = date.getMonth().toString();
+      let dateString = date.toLocaleDateString();
+      if (!dates[month]) {
+        dates[month] = [];
+      }
+      dates[month].push(dateString);
+    }
+
+    setDatesOfYear(dates);
+  }, []);
+
   return (
     <main className={styles.main}>
       <h2 className={styles.header}>The Year</h2>
       <div className={styles.months}>
-        {Array.from(Array(12).keys()).map((item) => {
+        {Array.from(Object.keys(datesOfYear)).map((item) => {
           return (
             <div className={styles.monthItem} key={item}>
               <span className={styles.monthName}>
-                {(item + 1).toString().padStart(2, "0")}
+                {(Number(item) + 1).toString().padStart(2, "0")}
               </span>
               <div className={styles.days}>
-                {Array.from(Array(30).keys()).map((item) => {
-                  return <div key={item}>🔺</div>;
+                {datesOfYear[item].map((item) => {
+                  return (
+                    <div data-date={item} key={item}>
+                      🔺
+                    </div>
+                  );
                 })}
               </div>
             </div>
